5c2498e1204a0fcef97860a4872cfd2c6d04abc5
MohammadAmin Forouzan <maf081378@gmail.com>
2023-09-21 14:42:31 +0330

    Update settings
    - configure required setting variables used in accounts' services

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 # DATABASES = {
 #     "drivername": "postgresql",
 #     "username": "postgres",
@@ -10,3 +12,10 @@ DATABASES = {
     "drivername": "sqlite",
     "database": "fast_store.db"
 }
+
+TOTP_EXPIRATION_TIME = timedelta(seconds=90)
+
+SECRET_KEY = '85bb38db2f01f7ab8fe3d7a5180008a8b6d8b8b126ab07c8178c8c5c536984e5'
+
+ACCESS_TOKEN_EXPIRE_TIME = timedelta(minutes=10)
+REFRESH_TOKEN_EXPIRE_TIME = timedelta(days=1)
