eb20c02631874d9af6c5f55a8697d4834be3516e
MohammadAmin Forouzan <maf081378@gmail.com>
2023-09-24 09:11:36 +0330

    Update accounts models
    - add additional fields

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -7,9 +7,10 @@ class User(FastModel):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-
-    email = Column(String, nullable=False)
+    email = Column(String(256), nullable=False, unique=True)
     username = Column(String(25))
+    first_name = Column(String(256), nullable=True)
+    last_name = Column(String(256), nullable=True)
 
     is_active = Column(Boolean, default=True)
     is_verified = Column(Boolean, default=False)
@@ -17,5 +18,6 @@ class User(FastModel):
     password = Column(String, nullable=False)
     totp_secret = Column(String)
 
+    last_login = Column(DateTime, nullable=True)
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, nullable=True, onupdate=func.now())
