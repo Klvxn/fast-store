a5f644da181bf5a904f600b2e49a44632565f214
MohammadAmin Forouzan <maf081378@gmail.com>
2023-09-26 05:06:03 +0330

    Update accounts managers
    - async to sync (I'm currently keeping everything sync other than routers because I have no sufficient knowledge for completely handling that)

--- apps/accounts/services/manager.py
+++ apps/accounts/services/manager.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
+from fastapi import HTTPException, status
+
 from . import totp
 from .hash import Hash
-from .. import exceptions
+from ..error_codes import AccountErrorCodes
 from ..models import User
 
 
@@ -8,21 +12,24 @@ class UserManager:
     Model = User
 
     @classmethod
-    async def get_user_by_email(cls, email: str):
+    def get_user_by_email(cls, email: str):
         return cls.Model.filter(cls.Model.email == email).first()
 
     @classmethod
-    async def get_user_by_id(cls, user_id: int):
+    def get_user_by_id(cls, user_id: int):
         return cls.Model.filter(cls.Model.id == user_id).first()
 
     @classmethod
-    async def update_last_login(cls, user_id: int):
+    def update_last_login(cls, user_id: int):
         User.update(user_id, last_login=datetime.utcnow())
 
     @classmethod
-    async def create_user(cls, data: dict) -> User:
-        if await cls.get_user_by_email(data["email"]):
-            raise exceptions.EmailInUse
+    def create_user(cls, data: dict) -> User:
+        if cls.get_user_by_email(data["email"]):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=AccountErrorCodes.EmailInUse
+            )
 
         data["password"] = Hash.hash_password(data["password"])
         data["totp_secret"] = totp.generate_secret()
@@ -30,31 +37,50 @@ class UserManager:
         return User.create(**data)
 
     @classmethod
-    async def authenticate(cls, data: dict) -> User:
-        user = await cls.get_user_by_email(data["email"])
+    def authenticate(cls, data: dict) -> User:
+        user = cls.get_user_by_email(data["email"])
 
         if not user or not Hash.verify(data["password"], user.password):
-            raise exceptions.InvalidCredentials
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=AccountErrorCodes.InvalidCredentials
+            )
 
         if not user.is_verified:
-            raise exceptions.UserNotVerified
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=AccountErrorCodes.UserNotVerified
+            )
 
         if not user.is_active:
-            raise exceptions.InactiveAccount
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=AccountErrorCodes.InactiveAccount
+            )
 
+        cls.update_last_login(user.id)
         return user
 
     @classmethod
-    async def verify(cls, data: dict) -> User:
-        user = await cls.get_user_by_email(data["email"])
+    def verify(cls, data: dict) -> User:
+        user = cls.get_user_by_email(data["email"])
 
         if not user or not user.is_active:
-            raise exceptions.InvalidVerification
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=AccountErrorCodes.InvalidVerification
+            )
 
         if user.is_verified:
-            raise exceptions.AlreadyVerified
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=AccountErrorCodes.AlreadyVerified
+            )
 
         if not totp.verify_totp(user.totp_secret, data["totp"]):
-            raise exceptions.InvalidTOTP
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=AccountErrorCodes.InvalidTOTP
+            )
 
         return User.update(user.id, is_verified=True, totp_secret=None)
