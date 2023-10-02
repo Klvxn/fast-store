3a18c711f42f08891fbccffe28e75cf42ca3d22a
MohammadAmin Forouzan <maf081378@gmail.com>
2023-10-02 06:10:08 +0330

    feat(accounts_models): Add basic RBAC model classes
    - add RBAC user fields (is_superuser, is_staff)
    - add group and permission models with m2m relation

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
-
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, Table, ForeignKey
+from sqlalchemy.orm import relationship
 from config.database import FastModel
 
 
@@ -14,6 +14,8 @@ class User(FastModel):
 
     is_active = Column(Boolean, default=True)
     is_verified = Column(Boolean, default=False)
+    is_superuser = Column(Boolean, default=False)
+    is_staff = Column(Boolean, default=False)
 
     password = Column(String, nullable=False)
     totp_secret = Column(String)
@@ -21,3 +23,37 @@ class User(FastModel):
     last_login = Column(DateTime, nullable=True)
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, nullable=True, onupdate=func.now())
+
+
+    # groups
+    # permissions
+    
+    # get_user_permissions
+    # get_group_permissions
+    # has_perm
+
+
+groups_permissions = Table(
+    "groups_permissions",
+    FastModel.metadata,
+    Column("permission_id", ForeignKey("permissions.id"), primary_key=True),
+    Column("group_id", ForeignKey("groups.id"), primary_key=True)
+)
+
+
+class Permission(FastModel):
+    __tablename__ = "permissions"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+
+    groups = relationship("Group", secondary=groups_permissions, back_populates="permissions")
+
+
+class Group(FastModel):
+    __tablename__ = "groups"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), unique=True)
+
+    permissions = relationship("Permission", secondary=groups_permissions, back_populates="groups")
